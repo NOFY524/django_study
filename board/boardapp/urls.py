f510fce31fc380c68d95db6988ce5c8ac7826227
NOFY <newbrother0204@gmail.com>
2022-10-08 17:56:22 +0900

    Delete function and like function

--- board/boardapp/urls.py
+++ board/boardapp/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_id>/', views.PostDetailView, name='detail'),
     path('post_upload/', views.PostUploadView, name='upload'),
-    path('<int:post_id>/comment_create/', views.CommentUploadView, name='comment_upload')
+    path('<int:post_id>/comment_create/', views.CommentUploadView, name='comment_upload'),
+    path('<int:post_id>/delete/', views.PostDeleteView, name='post_delete'),
+    path('<int:post_id>/<int:comment_id>/delete/', views.CommentDeleteView, name='comment_delete'),
+    path('<int:post_id>/like/', views.PostLikeView, name='post_like'),
+    path('<int:post_id>/<int:comment_id>/like', views.CommentLikeView, name='comment_like')
 ]
--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from .models import Post, Comment
 from .forms import PostUploadForm, CommentUploadForm
 
+from django.contrib.auth.decorators import  login_required
+
 # Create your views here.
 
 
@@ -21,6 +23,7 @@ def PostDetailView(request, post_id):
     return render(request, 'boardapp/post_detail.html', data)
 
 
+@login_required(login_url="accounts:login")
 def PostUploadView(request):
     if request.method == "GET":
         form = {"form": PostUploadForm()}
@@ -31,10 +34,13 @@ def PostUploadView(request):
         form = PostUploadForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            post.author = request.user
+
             post.save()
         return redirect('boardapp:index')
 
 
+@login_required(login_url="accounts:login")
 def CommentUploadView(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
 
@@ -42,6 +48,51 @@ def CommentUploadView(request, post_id):
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
+        comment.author = request.user
+
         comment.save()
 
+    return redirect('boardapp:detail', post_id=post_id)
+
+
+@login_required(login_url="accounts:login")
+def PostDeleteView(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user != post.author:
+        print('checked')
+        return redirect('boardapp:detail', post_id=post.id)
+    print('checked 2')
+    post.delete()
+    return redirect('boardapp:index')
+
+
+@login_required(login_url="accounts:login")
+def CommentDeleteView(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        return redirect('boardapp:detail', post_id=post_id)
+    comment.delete()
+    return redirect('boardapp:detail', post_id=post_id)
+
+
+@login_required(login_url='accounts:login')
+def PostLikeView(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.user in post.likes_user.all():
+        post.likes_user.remove(request.user)
+    else:
+        post.likes_user.add(request.user)
+
+    return redirect('boardapp:detail', post_id=post.id)
+
+
+@login_required(login_url='accounts:login')
+def CommentLikeView(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user in comment.likes_user.all():
+        comment.likes_user.remove(request.user)
+    else:
+        comment.likes_user.add(request.user)
     return redirect('boardapp:detail', post_id=post_id)
